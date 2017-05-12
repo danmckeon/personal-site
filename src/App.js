28394ab72a4545967ba1fd17cc73a2e440718cac
Dan McKeon <email": null>
2017-05-12 14:50:33 -0700

    create Nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
