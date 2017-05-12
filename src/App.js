f729a7ae75c80f21c185cbc78e38d6d3e459ba3c
Dan McKeon <email": null>
2017-05-12 14:53:55 -0700

    create intro component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './Nav';
+import Intro from './Intro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Intro />
       </div>
     );
   }
---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div className="intro">
+        <p>Intro will go here</p>
+      </div>
+    );
+  }
+}
+
+export default Intro;
