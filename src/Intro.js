77d2dabdfc086ecbb2367710a77d729e652df6e5
Dan McKeon <email": null>
2017-05-12 15:54:18 -0700

    add css to center intro text inside div; TODO: add right and left margin to center horizontally;

--- src/Intro.js
+++ src/Intro.js
@@ -4,8 +4,10 @@ class Intro extends Component {
   render() {
     return (
       <div className="intro">
-        <h4 className="intro-header">Dan McKeon</h4>
-        <p className="intro-text">I'm a software engineer and entrepreneur finding innovative solutions to fight cancer. I'm passionate about scalable technologies that empower people to help themselves. For the last year, I've been building a website to help people who have cancer access clinical trials. I recently attended Dev Bootcamp to take my web development skills to the next level.</p>
+        <div className="intro-text-container">
+          <h4 className="intro-header">Dan McKeon</h4>
+          <p className="intro-text">I'm a software engineer and entrepreneur finding innovative solutions to fight cancer. I'm passionate about scalable technologies that empower people to help themselves. For the last year, I've been building a website to help people who have cancer access clinical trials. I recently attended Dev Bootcamp to take my web development skills to the next level.</p>
+        </div>
       </div>
     );
   }
